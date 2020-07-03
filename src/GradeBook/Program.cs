a71f9ad93e29d4ba7f6ee0487298684dbf704d24
Andrew Miller <206alm@gmail.com>
2020-07-03 10:02:51 -0700

    added to program.cs, program logic and interaction

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -3,18 +3,59 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Program
+class Program 
     {
         static void Main(string[] args)
         {
-            var book = new Book("Current Gradebook");
-            book.AddGrade(99.9);
-            book.AddGrade(88.8);
-            book.AddGrade(77.7);
+            Book book = new Book("Andrew's Grade Book");
+
+            EnterGrades(book);
+
             var stats = book.GetStats();
-            Console.WriteLine($"Average: {stats.Average}");
-            Console.WriteLine($"High: {stats.High}");
-            Console.WriteLine($"LOw: {stats.Low}");
+
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            // Console.WriteLine($"The letter grade is {stats.Letter}");
         }
+
+        private static void EnterGrades(Book book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+
     }
 }
